67ff742e5c3974b920a80ae1b6914d7ad26e0cb1
M-Affan-Nazir <m.affann80@hotmail.com>
2024-07-25 18:54:31 -0600

    Adjust Dynamic Difference between Logos

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -21,17 +21,17 @@ export default function Languages(){
             </div>
             <div style={{display:"flex", flexDirection:"row", marginTop:"10px"}}>
               <img src={js} style={{height:"7vh", width:"7vw"}} />
-              <img src={ts} style={{height:"4vh", width:"4vw", marginTop:"17px", marginLeft:"-50px"}} />
-              <img src={python} style={{height:"4vh", width:"4vw", marginTop:"17px", marginLeft:"-30px"}} />
-              <img src={cpp} style={{height:"4vh", width:"2.7vw", marginTop:"17px", marginLeft:"-10px"}} />
-              <img src={java} style={{height:"7vh", width:"7vw", marginTop:"1px", marginLeft:"-40px"}} />
-              <img src={cs} style={{height:"4vh", width:"3vw", marginTop:"17px", marginLeft:"-50px"}} />
-              <img src={r} style={{height:"4vh", width:"3vw", marginTop:"17px", marginLeft:"15px"}} />
-              <img src={tf} style={{height:"4vh", width:"3vw", marginTop:"15px", marginLeft:"10px"}} />
-              <img src={pytorch} style={{height:"4vh", width:"7vw", marginTop:"19px", marginLeft:"10px"}} />
-              <img src={node} style={{height:"5vh", width:"4vw", marginTop:"11px", marginLeft:"10px"}} />
-              <img src={react} style={{height:"4vh", width:"3vw", marginTop:"17px", marginLeft:"10px"}} />
-              <img src={skl} style={{height:"5vh", width:"5vw", marginTop:"17px", marginLeft:"-10px"}} />
+              <img src={ts} style={{height:"4vh", width:"4vw", marginTop:"17px", marginLeft:"-3vw"}} />
+              <img src={python} style={{height:"4vh", width:"4vw", marginTop:"17px", marginLeft:"-1vw"}} />
+              <img src={cpp} style={{height:"4vh", width:"2.7vw", marginTop:"17px", marginLeft:"-0.5vw"}} />
+              <img src={java} style={{height:"7vh", width:"7vw", marginTop:"1px", marginLeft:"-2vw"}} />
+              <img src={cs} style={{height:"4vh", width:"3vw", marginTop:"17px", marginLeft:"-2.5vw"}} />
+              <img src={r} style={{height:"4vh", width:"3vw", marginTop:"17px", marginLeft:"-0vw"}} />
+              <img src={tf} style={{height:"4vh", width:"3vw", marginTop:"15px", marginLeft:"0.9vw"}} />
+              <img src={pytorch} style={{height:"4vh", width:"7vw", marginTop:"19px", marginLeft:"1vw"}} />
+              <img src={node} style={{height:"5vh", width:"4vw", marginTop:"11px", marginLeft:"1vw"}} />
+              <img src={react} style={{height:"4vh", width:"3vw", marginTop:"17px", marginLeft:"0.5vw"}} />
+              <img src={skl} style={{height:"5vh", width:"5vw", marginTop:"17px", marginLeft:"-0.5vw"}} />
             </div>
         </div>
     )
